3440313400042f5b3577aa5ab05c8eecc9d61122
chino4242 <ryanj.contino@gmail.com>
2025-09-05 09:27:58 -0400

    Finishes report functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import word_count, char_count, list_sort
 
 def get_book_text(filepath):
@@ -6,14 +7,19 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     char_dict = char_count(book_text)
     wc= word_count(book_text)
+    print(f"--- Begin report of {sys.argv[1]} ---")
     print(f"Found {wc} total words")
     sorted_list = list_sort(char_dict)
     for item in sorted_list:
         char = item["char"]
         num = item["num"]
         print(f"{char}: {num}")
+    print("--- End Report ---")
 
 main()
